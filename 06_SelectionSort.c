d00be311ede686f839c1ba6c67179cbda40bd03f
Bhagyashree Bhusare <bhusarebhagyashree6@gmail.com>
2026-08-14 13:55:25 +0530

    Fix selection sort implementation and output formatting

--- 06_SelectionSort.c
+++ 06_SelectionSort.c
@@ -1,38 +1,45 @@
-# Selection Sort
+# Selction Sort
 Code -
 #include <stdio.h>
 int main() {
-    int n, i, j, min, temp;
-    int arr[100];
-
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
-
-    printf("Enter %d elements:\n", n);
-    for (i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    // Write C code here
+    int a[100], n, i,j, min, temp;
+    printf("Enter number of elements:\n");
+    scanf("%d",&n);
+    printf("Enter %d elements:\n",n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
     }
-    for (i = 0; i < n - 1; i++) {
-        min = i;
-
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min]) {
-                min = j;
+    for(i=0; i<n-1; i++)
+    {
+        min =i;
+        for(j=i+1; j<n; j++)
+        {
+            if(a[min]>a[j])
+            {
+                min =j;
             }
         }
-        temp = arr[i];
-        arr[i] = arr[min];
-        arr[min] = temp;
+
+        if (min!=i)
+        {
+            int temp = a[min];
+            a[min] = a[i];
+            a[i]  =  temp;
+        }
     }
-    printf("Sorted array: ");
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+    printf("Sorted Array: ");
+    for(i=0; i<n; i++)
+    {
+        printf("%d ",a[i]);
     }
     return 0;
 }
-
 Output -
-Enter number of elements: 5
-Enter 5 elements:
+Enter number of elements:
+5
+Enter 5 elements: 
 64 25 12 22 11
-Sorted array: 11 12 22 25 64
+Sorted Array: 11 12 22 25 64
+
