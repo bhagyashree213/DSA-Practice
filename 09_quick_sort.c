305a02fffa53bf44bb2374be61799321a0a6a9d0
Bhagyashree Bhusare <bhusarebhagyashree6@gmail.com>
2026-08-17 15:56:57 +0530

    Refactor code to implement Quick Sort algorithm

--- 09_quick_sort.c
+++ 09_quick_sort.c
@@ -1,29 +1,23 @@
-# Stack using Array
+# Quick Sort
 Code -
 #include <stdio.h>
-
 void quickSort(int a[], int low, int high) {
     int i, j, pivot, temp;
-
     if (low < high) {
         pivot = a[low];
         i = low;
         j = high;
-
         while (i < j) {
             while (a[i] <= pivot && i < high)
                 i++;
-
             while (a[j] > pivot)
                 j--;
-
             if (i < j) {
                 temp = a[i];
                 a[i] = a[j];
                 a[j] = temp;
             }
         }
-
         temp = a[low];
         a[low] = a[j];
         a[j] = temp;
@@ -32,23 +26,17 @@ void quickSort(int a[], int low, int high) {
         quickSort(a, j + 1, high);
     }
 }
-
 int main() {
     int a[100], n, i;
-
     printf("Enter number of elements: ");
     scanf("%d", &n);
-
     printf("Enter elements:\n");
     for (i = 0; i < n; i++)
         scanf("%d", &a[i]);
-
     quickSort(a, 0, n - 1);
-
     printf("Sorted array:\n");
     for (i = 0; i < n; i++)
         printf("%d ", a[i]);
-
     return 0;
 }
 
