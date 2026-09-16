b17a9707820f78940eef4cf52cc7a57b32779713
Bhagyashree Bhusare <bhusarebhagyashree6@gmail.com>
2026-09-16 22:52:46 +0530

    Add Radix Sort implementation in C
    Implement Radix Sort algorithm for sorting integers.

---
+++ 31_RadixSort.c
@@ -0,0 +1,56 @@
+# Radix Sort
+Code -
+#include <stdio.h>
+int getMax(int arr[], int n)
+{
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max;
+}
+void countingSort(int arr[], int n, int exp)
+{
+    int output[n];
+    int count[10] = {0};
+    for (int i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+    for (int i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+    for (int i = n - 1; i >= 0; i--)
+    {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+void radixSort(int arr[], int n)
+{
+    int max = getMax(arr, n);
+    for (int exp = 1; max / exp > 0; exp *= 10)
+        countingSort(arr, n, exp);
+}
+int main()
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    radixSort(arr, n);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
+
+Output -
+Enter number of elements: 8
+Enter elements:
+170 45 75 90 802 24 2 66
+Sorted array: 2 24 45 66 75 90 170 802
