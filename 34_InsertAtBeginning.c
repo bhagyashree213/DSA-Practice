584db45218f376e51137ff895b45a50873368b30
Bhagyashree Bhusare <bhusarebhagyashree6@gmail.com>
2026-09-17 09:23:37 +0530

    Implement linked list insertion at beginning
    This program allows users to insert elements at the beginning of a linked list and displays the updated list.

---
+++ 34_InsertAtBeginning.c
@@ -0,0 +1,57 @@
+# Insert At Beginning
+Code -
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+struct Node* insertAtBeginning(struct Node *head, int value) {
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->next = head;
+
+    return newNode;
+}
+
+void display(struct Node *head) {
+    struct Node *temp = head;
+
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node *head = NULL;
+    int n, value;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter element: ");
+        scanf("%d", &value);
+        head = insertAtBeginning(head, value);
+    }
+
+    printf("\nLinked List after insertion at beginning:\n");
+    display(head);
+
+    return 0;
+}
+
+Output -
+Enter number of elements: 3
+Enter element: 10
+Enter element: 20
+Enter element: 30
+
+Linked List after insertion at beginning:
+30 -> 20 -> 10 -> NULL
